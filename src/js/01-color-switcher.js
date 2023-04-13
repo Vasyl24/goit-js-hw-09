6c4d76b96e7113149dc436b5ec98cb4e7a21ef3b
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-04-13 23:07:05 +0300

    Do 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let colorInterval = null;
+stop.disabled = true;
 
+start.addEventListener('click', () => {
+  start.disabled = true;
+  stop.disabled = false;
+
+  colorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stop.addEventListener('click', () => {
+  start.disabled = false;
+  stop.disabled = true;
+  clearInterval(colorInterval);
+});
